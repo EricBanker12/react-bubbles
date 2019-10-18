333647cb5da87b92f5401d5e7f9085fef9041c46
ericbanker12 <ericbanker12@gmail.com>
2019-10-18 10:30:47 -0500

    add input to login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,11 +3,45 @@ import React from "react";
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [input, setInput] = React.useState({
+    username: 'Lambda School',
+    password: 'i<3Lambd4',
+  })
+
+  function inputHandler(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  function submitHandler(e) {
+    e.preventDefault()
+    // send post to login
+  }
+
   return (
-    <>
+    <form onSubmit={submitHandler}>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <label>
+        Username:&nbsp;
+        <input
+          type='text'
+          name='username'
+          value={input.username}
+          onChange={inputHandler}
+        />
+      </label>
+      <label>
+        Password:&nbsp;
+        <input
+          type='text'
+          name='password'
+          value={input.password}
+          onChange={inputHandler}
+        />
+      </label>
+    </form>
   );
 };
 
